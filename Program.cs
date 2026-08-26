ccd88c29f7fc1bc3ee9f3ac4bcf432fa056b04de
Villads <vkus@itu.dk>
2026-08-26 18:35:28 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Bison.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.UserName);
+            Console.ReadLine();
+        }
+
+    }
+}
