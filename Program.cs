488dc7d2fac903cbcbac90d37cff8056057d2eda
Villads <vkus@itu.dk>
2026-08-27 11:53:35 +0200

    added basic command line input reading and printing of entire csv file

--- Program.cs
+++ Program.cs
@@ -1,11 +1,36 @@
-﻿namespace Bison.CLI
+﻿using System.IO;
+
+namespace Bison.CLI
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine(Environment.UserName);
-            Console.ReadLine();
+            if(args.Length > 0)
+            {
+                switch(args[0])
+                {
+                    case "read":
+                        ReadFromCSV();
+
+                        break;
+
+                    default:
+                        Console.WriteLine("command not recognized");
+                        break;
+                }
+            }
+        }
+
+        private static void ReadFromCSV()
+        {
+            StreamReader streamreader = new StreamReader("bison_observe_cli_db.csv");
+            while (streamreader.EndOfStream == false)
+            {
+                Console.WriteLine(streamreader.ReadLine());
+            }
+            streamreader.Close();
         }
 
     }
