cd2fe70755203c619e2844236ce97c45e0adc5ba
Limimums <enoe@itu.dk>
2026-09-03 11:00:16 +0200

    Refactor: Created SimpleDB library project for data persistence

---
+++ SimpleDB/CSVDatabase.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleDB
+{
+    public sealed class CSVDatabase<T> : IDatabaseRepository<T> //Implements the IDatabaseRepository. Sealed means no class can inherit from CDVDatabase
+    {
+        public IEnumerable<T> Read(int? limit = null)
+        {
+            
+        }
+        public void Store(T record)
+        {
+            
+        }
+    }
+}
+
