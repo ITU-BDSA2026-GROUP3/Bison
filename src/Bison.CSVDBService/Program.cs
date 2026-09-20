bbf209847d1622a9bc5d927984d4d52ab3ab86ec
Limimums <enoe@itu.dk>
2026-09-20 18:32:44 +0200

    Added getApp method to program

--- src/Bison.CSVDBService/Program.cs
+++ src/Bison.CSVDBService/Program.cs
@@ -1,50 +1,63 @@
+using System.Security.Cryptography.X509Certificates;
 using SimpleDB;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<IDatabaseRepository,CSVDatabase>();
-
-const string observationTableName = "bison_observe_cli_db";
-const string commentTableName = "bison_comment_cli_db";
-
-
-var app = builder.Build();
-IDatabaseRepository? database = app.Services.GetService<IDatabaseRepository>();
-
-database.DirectoryPath = Path.GetFullPath(
-    Path.Combine(AppContext.BaseDirectory, "../../../../../data"));
-database.CreateTable<ObservationRec>(observationTableName);
-database.CreateTable<CommentRec>(commentTableName);
-
-app.MapGet("/observations", () => database.Read<ObservationRec>(observationTableName));
-
-app.MapGet("/comments", (long id) => database.Read<CommentRec>(commentTableName).Where(comment => comment.obsID == id)); // needs to only include comments that match key
-
-app.MapPost("/comment", (CommentRec commentRec) =>
+namespace Bison.CSVDBService
 {
-    foreach (ObservationRec obs in database.Read<ObservationRec>(observationTableName)) // ensures an observation with that id exists before adding comment
+    public record ObservationRec(long obsID, string Author, string Observation, string Location, long Timestamp);
+    public record CommentRec(long obsID, string Comment);
+
+    public class Program
     {
-        if (obs.obsID == commentRec.obsID)
+        const string observationTableName = "bison_observe_cli_db";
+        const string commentTableName = "bison_comment_cli_db";
+
+        public void Main(String[] args)
         {
-            database.Store<CommentRec>(commentTableName, commentRec);
-            break;
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddSingleton<IDatabaseRepository,CSVDatabase>();
+            
+            var app = getApp(builder);
+
+            app.Run("http://localhost:5000");
+        }
+
+        public WebApplication getApp(WebApplicationBuilder builder)
+        {
+            var app = builder.Build();
+            IDatabaseRepository? database = app.Services.GetService<IDatabaseRepository>();
+
+            database.DirectoryPath = Path.GetFullPath(
+                Path.Combine(AppContext.BaseDirectory, "../../../../../data"));
+            database.CreateTable<ObservationRec>(observationTableName);
+            database.CreateTable<CommentRec>(commentTableName);
+
+            app.MapGet("/observations", () => database.Read<ObservationRec>(observationTableName));
+
+            app.MapGet("/comments", (long id) => database.Read<CommentRec>(commentTableName).Where(comment => comment.obsID == id)); // needs to only include comments that match key
+
+            app.MapPost("/comment", (CommentRec commentRec) =>
+            {
+                foreach (ObservationRec obs in database.Read<ObservationRec>(observationTableName)) // ensures an observation with that id exists before adding comment
+                {
+                    if (obs.obsID == commentRec.obsID)
+                    {
+                        database.Store<CommentRec>(commentTableName, commentRec);
+                        break;
+                    }
+                }
+                return Results.Ok();
+            });
+
+            app.MapPost("/observation", (ObservationRec observation) =>
+            {
+                if (string.IsNullOrWhiteSpace(observation.Author) || string.IsNullOrWhiteSpace(observation.Observation))
+                    return Results.BadRequest("Author and Observation cannot be empty.");
+
+                database.Store(observationTableName, observation);
+                return Results.Ok();
+            });
+
+            return app;
         }
     }
-    return Results.Ok();
-});
-
-app.MapPost("/observation", (ObservationRec observation) =>
-{
-    if (string.IsNullOrWhiteSpace(observation.Author) || string.IsNullOrWhiteSpace(observation.Observation))
-        return Results.BadRequest("Author and Observation cannot be empty.");
-
-    database.Store(observationTableName, observation);
-    return Results.Ok();
-});
-
-app.Run("http://localhost:5000");
-
-
-
-
-public record ObservationRec(long obsID, string Author, string Observation, string Location, long Timestamp);
-public record CommentRec(long obsID, string Comment);
+}
