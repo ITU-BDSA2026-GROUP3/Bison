4ac1485fee4f574b889d18dc9587be8f829df0d4
Villads <vkus@itu.dk>
2026-09-24 12:06:14 +0200

    Feature: early details page
    very bare bones, and only used for testing adding more pages.

--- src/Bison.Razor/BisonService.cs
+++ src/Bison.Razor/BisonService.cs
@@ -1,9 +1,11 @@
-public record ObservationViewModel(string Author, string Message, string Timestamp);
-
+public record ObservationViewModel(long obsID, string Author, string Message, string Location ,string Timestamp);
+public record CommentViewModel(long commentID, long obsID,string Author, string Comment);
 public interface IObservationService
 {
     public List<ObservationViewModel> GetObservations();
     public List<ObservationViewModel> GetObservationsFromAuthor(string author);
+
+    public List<CommentViewModel> GetComments(long id);
 }
 
 public class ObservationService : IObservationService
@@ -11,8 +13,13 @@ public class ObservationService : IObservationService
     // These would normally be loaded from a database for example
     private static readonly List<ObservationViewModel> _obs = new()
         {
-            new ObservationViewModel("Peter", "I saw a heron", UnixTimeStampToDateTimeString(1690892208)),
-            new ObservationViewModel("Paul", "There is a bison on Amager", UnixTimeStampToDateTimeString(1690895308)),
+            new ObservationViewModel(0, "Peter", "I saw a heron","Legoland", UnixTimeStampToDateTimeString(1690892208)),
+            new ObservationViewModel(1, "Paul", "There is a bison on Amager","Amager", UnixTimeStampToDateTimeString(1690895308)),
+        };
+    private static readonly List<CommentViewModel> _comments = new()
+        {
+            new CommentViewModel(0, 1,  "Peter","OMG where?"),
+            new CommentViewModel(1, 0, "Paul", "Nice observation bro!"),
         };
 
     public List<ObservationViewModel> GetObservations()
@@ -24,6 +31,11 @@ public class ObservationService : IObservationService
     {
         // filter by the provided author name
         return _obs.Where(x => x.Author == author).ToList();
+    }
+    public List<CommentViewModel> GetComments(long id)
+    {
+        // filter by the provided author name
+        return _comments.Where(x => x.obsID == id).ToList();
     }
 
     private static string UnixTimeStampToDateTimeString(double unixTimeStamp)
