6f1c4f13b5492f2cb1fbcde4fb7891398544743e
Villads <vkus@itu.dk>
2026-09-25 15:24:44 +0200

    Fix: CommentViewModel

--- src/Bison.Razor/BisonService.cs
+++ src/Bison.Razor/BisonService.cs
@@ -2,7 +2,7 @@ using System.Data;
 using static System.Net.Mime.MediaTypeNames;
 
 public record ObservationViewModel(long obsID, string Author, string Message, string Location ,string Timestamp);
-public record CommentViewModel(long commentID, long obsID,string Author, string Comment);
+public record CommentViewModel(long obsID, string Comment);
 public interface IObservationService
 {
     public List<ObservationViewModel> GetObservations();
@@ -21,8 +21,8 @@ public class ObservationService : IObservationService
         };
     private static readonly List<CommentViewModel> _comments = new()
         {
-            new CommentViewModel(0, 1,  "Peter","OMG where?"),
-            new CommentViewModel(1, 0, "Paul", "Nice observation bro!"),
+            new CommentViewModel(0,"OMG where?"),
+            new CommentViewModel(1,"Nice observation bro!"),
         };
 
     public List<ObservationViewModel> GetObservations()
@@ -46,7 +46,7 @@ public class ObservationService : IObservationService
         List<CommentViewModel> comments = new List<CommentViewModel>();
         foreach (IDataRecord row in DBFacade.ReadObservations())
         {
-            comments.Add(new CommentViewModel(row.GetInt64(0), row.GetInt64(1), row.GetString(2), row.GetString(3)));
+            comments.Add(new CommentViewModel(row.GetInt64(0),row.GetString(1)));
         }
 ;
         return _comments.Where(x => x.obsID == id).ToList();
